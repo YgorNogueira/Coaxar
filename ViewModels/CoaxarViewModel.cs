4a459e4393ea2684f38620dd10ce272ea1ed13fc
JoaoPedroAraujoSouza <joao.pedroaraujosouza@ufrpe.br>
2025-05-14 00:33:31 -0300

    feat: Implementação da ListView para ListPage

--- ViewModels/CoaxarViewModel.cs
+++ ViewModels/CoaxarViewModel.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CoaxarApp.ViewModels
@@ -10,5 +12,10 @@ namespace CoaxarApp.ViewModels
     public class CoaxarViewModel
     {
         public ObservableCollection<AnimalModel> Animals { get; set; } = new ObservableCollection<AnimalModel>();
+
+        public IEnumerable<string> Families => Animals
+            .GroupBy(Animals => Animals.Family)
+            .Select(group => group.Key)
+            .Where(family => !string.IsNullOrEmpty(family));
     }
 }
